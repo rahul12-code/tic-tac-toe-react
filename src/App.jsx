fadb99686468aa51eebf2c6a022bbf5130cf1fb4
rahul <rahulkandati@gmail.com>
2024-12-21 12:46:00 +0530

    initialized react app with vite

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-[100px] text-red-800 italic'>
+      TIC TAC TOE REACT
+    </div>
+  )
+}
+
+export default App
